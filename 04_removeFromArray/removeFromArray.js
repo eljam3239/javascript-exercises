555b876bc10762538d25b2e89b1b3f441c887fa1
Eli <elijames2301@gmail.com>
2024-09-16 00:06:06 -0400

    Write 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newArray = [];
+    for (let i =0;i<arr.length;i++){
+        if (args.includes(arr[i])){
+            continue;
+        }
+        newArray.push(arr[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
